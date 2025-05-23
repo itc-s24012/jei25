59fc0f0cb45f93ace2beb8a38d4e5f4ebb668bb4
itc-s24012 <s24012@std.it-college.ac.jp>
2025-05-23 12:03:25 +0900

    課題

---
+++ D49Q20.java
@@ -0,0 +1,7 @@
+public class D49Q20 {
+    public static void main(String[] args) {
+        double[][] arr = new double[5][10];
+        System.out.println("行数: " + arr.length);
+        System.out.println("列数: " + arr[0].length);
+    }
+}
